5872b6e7b0568c0610ebcecdf409f09a7edeb243
Romildo Jozué Paiter <romildopaiter@gmail.com>
2012-08-02 19:18:23 -0400

    Arquivos Realizados na Cepromat

--- src/main/java/br/com/grupopaiter/sofistcarros/entity/Loja.java
+++ src/main/java/br/com/grupopaiter/sofistcarros/entity/Loja.java
@@ -3,6 +3,8 @@ package br.com.grupopaiter.sofistcarros.entity;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Version;
 
 import org.jboss.seam.annotations.Name;
 
@@ -10,15 +12,38 @@ import org.jboss.seam.annotations.Name;
 @Name(value="loja")
 public class Loja {
 	
+	@Id
+	private long idLoja;
+		
+	// private List<Veiculo> veiculos;
 	
-	private List<Veiculo> veiculos;
+	
+	@Version
+	private Integer version;
+	
 
-	public List<Veiculo> getVeiculos() {
+/*	public List<Veiculo> getVeiculos() {
 		return veiculos;
 	}
 
 	public void setVeiculos(List<Veiculo> veiculos) {
 		this.veiculos = veiculos;
+	}*/
+
+	public long getIdLoja() {
+		return idLoja;
+	}
+
+	public void setIdLoja(long idLoja) {
+		this.idLoja = idLoja;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
 	}
 
 }
--- src/main/java/br/com/grupopaiter/sofistcarros/entity/Veiculo.java
+++ src/main/java/br/com/grupopaiter/sofistcarros/entity/Veiculo.java
@@ -11,19 +11,35 @@ import org.jboss.seam.annotations.Name;
 public class Veiculo {
 
 	@Id
+	private long idVeiculo;
 	
-	
-	private Loja loja;
+	//private Loja loja;
 	
 	@Version
 	private Integer version;
 
-	public Loja getLoja() {
+/*	public Loja getLoja() {
 		return loja;
 	}
 
 	public void setLoja(Loja loja) {
 		this.loja = loja;
+	}*/
+
+	public long getIdVeiculo() {
+		return idVeiculo;
+	}
+
+	public void setIdVeiculo(long idVeiculo) {
+		this.idVeiculo = idVeiculo;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
 	}
 	
 	
